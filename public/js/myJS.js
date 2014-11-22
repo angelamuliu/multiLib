52ff12d76c4d749d63a53e06a78572e83fe5cc45
Angela Liu <angelamuliu@gmail.com>
2014-11-21 22:56:24 -0500

    Added robot image, fixed styling on home page to be 2 column on wider screens

--- public/js/myJS.js
+++ public/js/myJS.js
@@ -1,4 +1,11 @@
 
 // Storing the different views that can be rendered here
 
-var homeNode = "<div id=\"home\"><h1>multi<strong>LIB</strong></h1><ul id=\"home\"><li>Fill in wacky stories together with your friends online! As a host, you can choose which story template to use and choose from player input, which word goes in the final story. As players, pitch in words as the host asks!</li><li><button onclick=\"setHost()\">Host a game!</button></li></ul></div>";
+// var homeNode = "<div id=\"home\"><h1>multi<strong>LIB</strong></h1><ul id=\"home\"><li>Fill in wacky stories together with your friends online! As a host, you can choose which story template to use and choose from player input, which word goes in the final story. As players, pitch in words as the host asks!</li><li><button onclick=\"setHost()\">Host a game!</button></li></ul></div>";
+
+
+var homeNode = "<div id=\"home\">" +
+	"<h1>multi<strong>LIB</strong></h1>" +
+	"<p>Fill in wacky stories together with your friends online! As a host, you can choose which story template to use and choose from player input, which word goes in the final story. As players, pitch in words as the host asks!</p>" +
+	"<button onclick=\"setHost()\" >Host a game!</button></div>" +
+    "<img id=\"robos\" src=\"images/robos.png\"/></div>";
