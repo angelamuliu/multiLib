64c7f3d4f9614181ed1f827efb6eaf5d248e93b6
Angela Liu <angelamuliu@gmail.com>
2014-12-02 11:32:08 -0500

    Changes to mongo file to allow connection to openshift mongo db

--- models/mongo.js
+++ models/mongo.js
@@ -8,7 +8,23 @@ var BSON = require('mongodb').BSONPure; // Used to locate docs by doc id
  * The typical mongodb port is 27012
  * The path part (here "fallTest") is the name of the databas
  */
-var url = 'mongodb://localhost:27017/libs';
+
+
+ // Default to localhost connection
+var url = 'mongodb://127.0.0.1:27017/libs';
+
+// if openshift env variables are present, we need to connect to Openshift's mongo
+// end result: 'mongodb://user:password@host:port/app_name' 
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  url = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+
+console.log(url);
+
 var mongoDB; // The connected database
 
 // Use connect method to connect to the Server
